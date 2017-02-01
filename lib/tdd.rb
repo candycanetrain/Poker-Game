338f17ab50ed777723dd865685a271af77750e65
Candra_Tran <candratran@gmail.com>
2017-02-01 11:47:57 -0800

    Add two_sum and my_transpose test methods

---
+++ lib/tdd.rb
@@ -0,0 +1,24 @@
+require "byebug"
+
+class Array
+  def my_uniq
+    result = []
+    self.each do |el|
+      result << el unless result.include?(el)
+    end
+    result
+  end
+
+  def two_sum
+    result = []
+    self.each_with_index do |el, i|
+      raise "Contents are not numbers." unless el.is_a?(Integer)
+      ((i+1)...self.length).each do |j|
+        result << [i,j] if el+self[j] == 0
+      end
+    end
+    result
+  end
+
+
+end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,21 +1,59 @@
 require "rspec"
 require "tdd"
 
-describe Array do
-  let(:arr) {[1,2,3,4,4,5,3]}
 
+
+describe Array do
   describe "#my_uniq" do
     it "returns a unique array" do
-      expect(arr.my_uniq).to eq([1,2,3,4,5])
+      expect([1,2,3,4,4,5,3].my_uniq).to eq([1,2,3,4,5])
     end
 
     it "returns an empty when called on empty array" do
       expect([].my_uniq).to eq([])
     end
 
-    it "returns self if self is already unique" do
+    it "returns copy if self is already unique" do
       expect([1,2,3].my_uniq).to eq([1,2,3])
-    end    
+    end
+    it "returns a new array; not self" do
+      expect([1,2,3].my_uniq).not_to be([1,2,3])
+    end
+
   end
 
+
+  describe "#two_sum" do
+    it "returns a blank array if self is empty" do
+      expect([].two_sum).to eq([])
+    end
+
+    it "returns an array of pair of indices where the elements add up to zero" do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+    it "returns a blank array if no pairs add up to zero" do
+      expect([1,2,3,4].two_sum).to eq([])
+    end
+
+    it "raises error when contents are not numbers" do
+      expect{["words", "hello", "ruby"].two_sum}.to raise_error("Contents are not numbers.")
+    end
+  end
+
+  describe "#my_transpose" do
+      let(:rows) {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ]}
+      let(:cols) {[
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ]}
+    it "transposes rows and columns" do
+      expect(rows.my_transpose).to eq(cols)
+    end
+  end
 end
