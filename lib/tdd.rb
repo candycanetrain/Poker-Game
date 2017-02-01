85803cdea95b88f3cfff18e615922bffecc433e3
Candra_Tran <candratran@gmail.com>
2017-02-01 14:26:06 -0800

    Finish Test Driven Developent exercise

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -33,3 +33,44 @@ class Array
   end
 
 end
+
+def stock_picker(stock_prices)
+  profit = 0
+  result = []
+  stock_prices.each_with_index do |buy, i|
+    stock_prices.each_with_index do |sell, j|
+      if i < j
+        temp_profit = sell - buy
+        if temp_profit > profit
+          profit = temp_profit
+          result = [i,j]
+        end
+      end
+    end
+  end
+
+  result
+
+end
+
+class TowersofHanoi
+  attr_reader :towers
+  def initialize
+    @towers = [[3,2,1],[],[]]
+  end
+
+  def move(start, last)
+    if @towers[start].empty?
+      return nil
+    elsif @towers[last].empty? && @towers[start].count > 0
+      @towers[last] << @towers[start].pop
+    elsif @towers[start].last > @towers[last].last
+      raise "Not valid move"
+    end
+  end
+
+
+  def won?
+    @towers[1].count == 3 || @towers[2].count == 3
+  end
+end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -61,18 +61,59 @@ end
 
 describe "Stock Picker" do
   it "returns most profitable pair of days" do
-    expect(stock_picker([4,5,1,7,4,6,9]).to eq([2,6])
+    expect(stock_picker([4,5,1,7,4,6,9])).to eq([2,6])
   end
 
   it "returns empty array if no profitable pairs" do
-    expect(stock_picker([6,5,4,3,2,1]).to eq([])
+    expect(stock_picker([6,5,4,3,2,1])).to eq([])
   end
 
   it "returns empty array if no profitable pairs" do
-    expect(stock_picker([4,4,4,4]).to eq([])
+    expect(stock_picker([4,4,4,4])).to eq([])
   end
 
   it "returns first pair of profitable days if there are two equal pairs" do
-    expect(stock_picker([1,9,4,5,1,2,9]).to eq([0,1])
+    expect(stock_picker([1,9,4,5,1,2,9])).to eq([0,1])
+  end
+end
+
+describe "Towers of Hanoi" do
+
+  subject(:game) {TowersofHanoi.new()}
+  describe "#initialization" do
+
+    it "creates three towers in array" do
+      expect(game.towers).to eq([[3,2,1],[],[]])
+    end
+  end
+
+  describe "#move" do
+    it "takes in two positions as arguments" do
+      expect{game.move(0,2)}.not_to raise_error(ArgumentError)
+    end
+
+    it "doesn't let you move a bigger disc to a smaller disc" do
+      game.move(0,1)
+      expect{game.move(0,1)}.to raise_error("Not valid move")
+    end
+    it "moves a disc from a tower to another tower" do
+      game.move(0,1)
+      expect(game.towers).to eq([[3,2],[1],[]])
+    end
+  end
+
+
+  describe "#won?" do
+    let(:towers) {[[],[3,2,1],[]]}
+
+    it "returns true when all discs are moved to tower2 or tower3" do
+      game.towers[0] = []
+      game.towers[2] = [3,2,1]
+      expect(game.won?).to eq(true)
+    end
+
+    it "returns false if all discs are not in one tower" do
+      expect(game.won?).to eq(false)
+    end
   end
 end
