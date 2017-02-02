587ac085b9e2e70feadc56f5db56533e0aeed690
Candra_Tran <candratran@gmail.com>
2017-02-01 17:51:56 -0800

    Working on hand.rb/ranking system

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,10 +1,45 @@
 require 'deck'
 
+
+ROYAL_FLUSH = [:A, :K, :Q, :J, :"10"]
+FACE_CARDS = [:A, :K, :Q, :J]
+
 class Hand
   attr_accessor :deck, :cards_in_hand
   def initialize(deck)
     @deck = deck
     @cards_in_hand = @deck.take_five
 
+  end
+
+  def get_ranking
+    card_symbols = get_symbols
+    return 1 if card_symbols.all?{|card| ROYAL_FLUSH.include?(card)}
+    hearts = 0
+    spades = 0
+
+    match_occurences = {
+      key => occurences (default 0)
+    }
+
+
+
+    card_symbols.each do |symbol|
+      match_occurences[symbol] += 1
+
+    end
+
+    match_occurences.keys.select do |k,v| 
+      value > 1
+
+    end
+  end
+
+  def get_symbols
+    card_symbols = []
+    cards_in_hand.each do |card|
+      card_symbols << card.symbol
+    end
+    card_symbols
   end
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -3,14 +3,27 @@ require "hand"
 require "deck"
 
 describe "Hand" do
-
+  let(:deck) {Deck.new}
   subject(:hand) {Hand.new(deck)}
-  let(:deck) {Deck.new()}
+
 
   describe "#initialize" do
     it "takes five from the deck" do
       expect(hand.cards_in_hand.count).to eq(5)
     end
+  end
+
+  describe "#get_ranking" do
+    it "returns the rank for the hand" do
+      expect(hand.get_ranking.class).to eq(Integer)
+    end
+
+    # it "returns 1 for straight flush" do
+    #   Card.new("Ace", :A, "spade", "black")
+    #   hand.cards_in_hand = []
+    # end
+  end
+
   end
 
 
